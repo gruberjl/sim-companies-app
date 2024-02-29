c1fcc7809962462d107e3e936843b069d15cd5cf
John Gruber <gruberjl@gmail.com>
2024-02-28 22:11:47 -0500

    updated sales office page

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,12 @@ import './pure-min.css'
 
 function Term({term, setTerm}) {
 	const setThePrice = (product) => (e) => {
-		term.price[product] = e.currentTarget.value
+		term[product].price = e.currentTarget.value
 		setTerm(term)
 	}
 
 	const setQuantity = (product) => (e) => {
-		term.quantity[product] = e.currentTarget.value
+		term[product].quantity = e.currentTarget.value
 		setTerm(term)
 	}
 
@@ -25,45 +25,47 @@ function Term({term, setTerm}) {
 		setTerm(term)
 	}
 
+	const deleteTerm = () => {
+		setTerm(null)
+	}
+
 	return (
-		<div>
-			<Header currentPage={'/'}/>
-			<div className="pure-form term-container">
-				<div className="product-term">
-					<input value={term.wages} onInput={setWages} placeholder="Wages" type="number" style={{height:'34px'}} className="margin6"/>
-					<input value={term.qualityBonus} onInput={setQualityBonus} placeholder="Quality Bonus %" type="number" style={{height:'34px'}} className="margin6"/>
-				</div>
-				<div className="product-term">
-					<img src="/public/SOR.png" height="40" width="40" alt="Sub-orbital rocket" className="margin6"/>
-					<input value={term.price.sor} onInput={setThePrice('sor')} placeholder="$" type="number" className="margin6"/>
-					<input value={term.quantity.sor} onInput={setQuantity('sor')} placeholder="# Quantity" type="number" className="margin6"/>
-				</div>
-				<div className="product-term">
-					<img src="/public/BFR.png" height="40" width="40" alt="BFR" className="margin6"/>
-					<input value={term.price.bfr} onInput={setThePrice('bfr')} placeholder="$" type="number" className="margin6"/>
-					<input value={term.quantity.bfr} onInput={setQuantity('bfr')} placeholder="# Quantity" type="number" className="margin6"/>
-				</div>
-				<div className="product-term">
-					<img src="/public/jumbo.png" height="40" width="40" alt="Jumbo jet" className="margin6"/>
-					<input value={term.price.jumbo} onInput={setThePrice('jumbo')} placeholder="$" type="number" className="margin6"/>
-					<input value={term.quantity.jumbo} onInput={setQuantity('jumbo')} placeholder="# Quantity" type="number" className="margin6"/>
-				</div>
-				<div className="product-term">
-					<img src="/public/luxury.png" height="40" width="40" alt="Luxury jet" className="margin6"/>
-					<input value={term.price.lux} onInput={setThePrice('lux')} placeholder="$" type="number" className="margin6"/>
-					<input value={term.quantity.lux} onInput={setQuantity('lux')} placeholder="# Quantity" type="number" className="margin6"/>
-				</div>
-				<div className="product-term">
-					<img src="/public/sep.png" height="40" width="40" alt="Single Engine Plane" className="margin6"/>
-					<input value={term.price.sep} onInput={setThePrice('sep')} placeholder="$" type="number" className="margin6"/>
-					<input value={term.quantity.sep} onInput={setQuantity('sep')} placeholder="# Quantity" style={{height:'34px'}} type="number" className="margin6"/>
-				</div>
-				<div className="product-term">
-					<img src="/public/satellite.png" height="40" width="40" alt="Satellite" className="margin6"/>
-					<input value={term.price.sat} onInput={setThePrice('sat')} placeholder="$" type="number" className="margin6"/>
-					<input value={term.quantity.sat} onInput={setQuantity('sat')} placeholder="# Quantity" type="number" className="margin6"/>
-				</div>
-			</div>
+		<div className="pure-form">
+			<fieldset>
+				<input value={term.wages} onInput={setWages} placeholder="Wages" type="number"/>
+				<input value={term.qualityBonus} onInput={setQualityBonus} placeholder="Quality Bonus %" type="number"/>
+				<button className="button-delete pure-button" onClick={deleteTerm}>X</button>
+			</fieldset>
+			<fieldset>
+				<img src="/public/sor.png" height="40" width="40" alt="Sub-orbital rocket" className="align-middle"/>
+				<input value={term.sor.price} onInput={setThePrice('sor')} placeholder="$" type="number"/>
+				<input value={term.sor.quantity} onInput={setQuantity('sor')} placeholder="# Quantity" type="number"/>
+			</fieldset>
+			<fieldset>
+				<img src="/public/bfr.png" height="40" width="40" alt="BFR" className="align-middle"/>
+				<input value={term.bfr.price} onInput={setThePrice('bfr')} placeholder="$" type="number"/>
+				<input value={term.bfr.quantity} onInput={setQuantity('bfr')} placeholder="# Quantity" type="number"/>
+			</fieldset>
+			<fieldset>
+				<img src="/public/jumbo.png" height="40" width="40" alt="Jumbo jet" className="align-middle"/>
+				<input value={term.jumbo.price} onInput={setThePrice('jumbo')} placeholder="$" type="number"/>
+				<input value={term.jumbo.quantity} onInput={setQuantity('jumbo')} placeholder="# Quantity" type="number"/>
+			</fieldset>
+			<fieldset>
+				<img src="/public/lux.png" height="40" width="40" alt="Luxury jet" className="align-middle"/>
+				<input value={term.lux.price} onInput={setThePrice('lux')} placeholder="$" type="number"/>
+				<input value={term.lux.quantity} onInput={setQuantity('lux')} placeholder="# Quantity" type="number"/>
+			</fieldset>
+			<fieldset>
+				<img src="/public/sep.png" height="40" width="40" alt="Single Engine Plane" className="align-middle"/>
+				<input value={term.sep.price} onInput={setThePrice('sep')} placeholder="$" type="number"/>
+				<input value={term.sep.quantity} onInput={setQuantity('sep')} placeholder="# Quantity" type="number"/>
+			</fieldset>
+			<fieldset>
+				<img src="/public/sat.png" height="40" width="40" alt="Satellite" className="align-middle"/>
+				<input value={term.sat.price} onInput={setThePrice('sat')} placeholder="$" type="number"/>
+				<input value={term.sat.quantity} onInput={setQuantity('sat')} placeholder="# Quantity" type="number"/>
+			</fieldset>
 		</div>
 	);
 }
@@ -84,26 +86,36 @@ function Contract({contract, setContract}) {
 		setContract(contract)
 	}
 
-	return (<div>
-		<div className="flex margin6-0" style={{height:'40px'}}>
-			{ contract.product == 'sep' ? <img src="/public/sep.png" height="40" width="40" alt="Single Engine Plane"/> : undefined }
-			{ contract.product == '' ? <div style={{width:'40px'}}/> : undefined }
-			
-			<select name="product" value={contract.product} onChange={setProduct} placeholder="Product">
-  			<option value="sep">Single Engine Plane</option>
-			</select>
-			<div className="margin0-6">
-				<input value={contract.price} onInput={setPrice} placeholder="Price ($)" style={{height:'34px'}} type="number"/>
-			</div>
-			<div className="margin0-6">
-				<input value={contract.quality} onInput={setQuality} placeholder="Quality" style={{height:'34px'}} type="number"/>
-			</div>
-			<div>
-				<div>{contract.highestProfit}</div>
-				<div>{}</div>
-			</div>
+	const deleteContract = () => {
+		setContract(null)
+	}
+
+	return (
+		<div className="pure-form">
+			<fieldset>
+				{ contract.product == 'sep' ? <img src="/public/sep.png" height="40" width="40" alt="Single Engine Plane" className="align-middle"/> : undefined }
+				{ contract.product == 'sat' ? <img src="/public/sat.png" height="40" width="40" alt="Satellite" className="align-middle"/> : undefined }
+				{ contract.product == 'lux' ? <img src="/public/lux.png" height="40" width="40" alt="Luxury jet" className="align-middle"/> : undefined }
+				{ contract.product == 'jumbo' ? <img src="/public/jumbo.png" height="40" width="40" alt="Jumbo jet" className="align-middle"/> : undefined }
+				{ contract.product == 'bfr' ? <img src="/public/bfr.png" height="40" width="40" alt="BFR" className="align-middle"/> : undefined }
+				{ contract.product == 'sor' ? <img src="/public/sor.png" height="40" width="40" alt="Sub-orbital rocket" className="align-middle"/> : undefined }
+				{ contract.product == '' ? <div className="box40"/> : undefined }
+				
+				<select name="product" value={contract.product} onChange={setProduct} placeholder="Product">
+					<option value="sep">Single Engine Plane</option>
+					<option value="sat">Satellite</option>
+					<option value="lux">Luxury jet</option>
+					<option value="jumbo">Jumbo jet</option>
+					<option value="bfr">BFR</option>
+					<option value="sor">Sub-orbital rocket</option>
+				</select>
+				<input value={contract.price} onInput={setPrice} placeholder="Price ($)" type="number"/>
+				<input value={contract.quality} onInput={setQuality} placeholder="Quality" type="number"/>
+				<span>{contract.highestProfit || 'profit'}</span>
+				<button className="button-delete pure-button" onClick={deleteContract}>X</button>
+			</fieldset>
 		</div>
-	</div>)
+	)
 }
 
 export function App() {
@@ -112,24 +124,30 @@ export function App() {
 
 	const setTerm = (idx) => (term) => {
 		const newTerms = [...terms]
-		newTerms[idx] = term
+
+		if (term == null) {
+			newTerms.splice(idx, 1)
+		} else {
+			newTerms[idx] = term
+		}
+		
+		const newContracts = contracts.map(contract => {
+			calculateProfitForContract(contract, newTerms)
+			return contract
+		})
+		setContracts(newContracts)
 		setTerms(newTerms)
 	}
 
-	const calculateProfitForContract = (contract) => {
+	const calculateProfitForContract = (contract, newTerms) => {
 		contract.highestProfit = undefined
-
 		if (contract.product && contract.quality && contract.price) {
-			terms.forEach(term => {
-				if (term.quantity[contract.product] && term.price[contract.product] && term.qualityBonus) {
-					const price = Number(term.price[contract.product])
+			newTerms.forEach(term => {
+				if (term[contract.product].price && term.qualityBonus) {
+					const price = Number(term[contract.product].price)
 					const qualityBonus = (term.qualityBonus/100)*price*contract.quality
 					const contractPrice = contract.price
 					const profit = price+qualityBonus-contractPrice
-					console.log(typeof price)
-					console.log(typeof qualityBonus)
-					console.log(typeof contractPrice)
-					console.log(typeof profit)
 					if (contract.highestProfit === undefined || profit > contract.highestProfit)
 						contract.highestProfit = profit
 				}
@@ -138,10 +156,16 @@ export function App() {
 	}
 
 	const setContract = idx => (contract) => {
-		calculateProfitForContract(contract)
 		const newContracts = [...contracts]
-		newContracts[idx] = contract
-		setContracts(newContracts)
+		if (contract == null) {
+			newContracts.splice(idx, 1)
+			setContracts(newContracts)
+		} else {
+			calculateProfitForContract(contract, terms)
+			newContracts[idx] = contract
+			setContracts(newContracts)
+		}
+
 	}
 
 	const addTerm = () => {
@@ -149,22 +173,12 @@ export function App() {
 		newTerms.push({
 			qualityBonus: undefined,
 			wages: undefined,
-			quantity: {
-				sep: '',
-				sat: '',
-				lux: '',
-				jumbo: '',
-				bfr: '',
-				sor: '',
-			},
-			price: {
-				sep: '',
-				sat: '',
-				lux: '',
-				jumbo: '',
-				bfr: '',
-				sor: '',
-			}
+			sep: {price: '', quantity: ''},
+			sat: {price: '', quantity: ''},
+			lux: {price: '', quantity: ''},
+			jumbo: {price: '', quantity: ''},
+			bfr: {price: '', quantity: ''},
+			sor: {price: '', quantity: ''},
 		})
 
 		setTerms(newTerms)
@@ -183,21 +197,24 @@ export function App() {
 	}
 
 	return (
-		<main className="margin6">
-			<h1>Sim Companies Sales Office calculator</h1>
-			<section>
-				{ terms.map((term, idx) => (
-					<Term term={term} setTerm={setTerm(idx)} key={idx}/>
-				))}
-				<button onClick={addTerm}>Add Term</button>
-			</section>
-			<section>
-				{ contracts.map((contract, idx) => (
-					<Contract contract={contract} setContract={setContract(idx)} key={idx}/>
-				))}
-				<button onClick={addContract}>Add Contract</button>
-			</section>
-		</main>
+		<div>
+			<Header currentPage={'/'}/>
+			<main>
+				<h1>Sim Companies Sales Office calculator</h1>
+				<section>
+					{ terms.map((term, idx) => (
+						<Term term={term} setTerm={setTerm(idx)} key={idx}/>
+					))}
+					<button className="pure-button pure-button-primary" onClick={addTerm}>Add Term</button>
+				</section>
+				<section>
+					{ contracts.map((contract, idx) => (
+						<Contract contract={contract} setContract={setContract(idx)} key={idx}/>
+					))}
+					<button className="pure-button pure-button-primary" onClick={addContract}>Add Contract</button>
+				</section>
+			</main>
+		</div>
 	);
 }
 
