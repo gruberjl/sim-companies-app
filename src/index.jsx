25f5a446ccbc5ac19554b6582c820a897891e213
John Gruber <gruberjl@gmail.com>
2024-02-28 22:23:44 -0500

    fixing images

--- src/index.jsx
+++ src/index.jsx
@@ -37,32 +37,32 @@ function Term({term, setTerm}) {
 				<button className="button-delete pure-button" onClick={deleteTerm}>X</button>
 			</fieldset>
 			<fieldset>
-				<img src="/sor.png" height="40" width="40" alt="Sub-orbital rocket" className="align-middle"/>
+				<img src="sor.png" height="40" width="40" alt="Sub-orbital rocket" className="align-middle"/>
 				<input value={term.sor.price} onInput={setThePrice('sor')} placeholder="$" type="number"/>
 				<input value={term.sor.quantity} onInput={setQuantity('sor')} placeholder="# Quantity" type="number"/>
 			</fieldset>
 			<fieldset>
-				<img src="/bfr.png" height="40" width="40" alt="BFR" className="align-middle"/>
+				<img src="bfr.png" height="40" width="40" alt="BFR" className="align-middle"/>
 				<input value={term.bfr.price} onInput={setThePrice('bfr')} placeholder="$" type="number"/>
 				<input value={term.bfr.quantity} onInput={setQuantity('bfr')} placeholder="# Quantity" type="number"/>
 			</fieldset>
 			<fieldset>
-				<img src="/jumbo.png" height="40" width="40" alt="Jumbo jet" className="align-middle"/>
+				<img src="jumbo.png" height="40" width="40" alt="Jumbo jet" className="align-middle"/>
 				<input value={term.jumbo.price} onInput={setThePrice('jumbo')} placeholder="$" type="number"/>
 				<input value={term.jumbo.quantity} onInput={setQuantity('jumbo')} placeholder="# Quantity" type="number"/>
 			</fieldset>
 			<fieldset>
-				<img src="/lux.png" height="40" width="40" alt="Luxury jet" className="align-middle"/>
+				<img src="lux.png" height="40" width="40" alt="Luxury jet" className="align-middle"/>
 				<input value={term.lux.price} onInput={setThePrice('lux')} placeholder="$" type="number"/>
 				<input value={term.lux.quantity} onInput={setQuantity('lux')} placeholder="# Quantity" type="number"/>
 			</fieldset>
 			<fieldset>
-				<img src="/sep.png" height="40" width="40" alt="Single Engine Plane" className="align-middle"/>
+				<img src="sep.png" height="40" width="40" alt="Single Engine Plane" className="align-middle"/>
 				<input value={term.sep.price} onInput={setThePrice('sep')} placeholder="$" type="number"/>
 				<input value={term.sep.quantity} onInput={setQuantity('sep')} placeholder="# Quantity" type="number"/>
 			</fieldset>
 			<fieldset>
-				<img src="/sat.png" height="40" width="40" alt="Satellite" className="align-middle"/>
+				<img src="sat.png" height="40" width="40" alt="Satellite" className="align-middle"/>
 				<input value={term.sat.price} onInput={setThePrice('sat')} placeholder="$" type="number"/>
 				<input value={term.sat.quantity} onInput={setQuantity('sat')} placeholder="# Quantity" type="number"/>
 			</fieldset>
@@ -93,12 +93,12 @@ function Contract({contract, setContract}) {
 	return (
 		<div className="pure-form">
 			<fieldset>
-				{ contract.product == 'sep' ? <img src="/sep.png" height="40" width="40" alt="Single Engine Plane" className="align-middle"/> : undefined }
-				{ contract.product == 'sat' ? <img src="/sat.png" height="40" width="40" alt="Satellite" className="align-middle"/> : undefined }
-				{ contract.product == 'lux' ? <img src="/lux.png" height="40" width="40" alt="Luxury jet" className="align-middle"/> : undefined }
-				{ contract.product == 'jumbo' ? <img src="/jumbo.png" height="40" width="40" alt="Jumbo jet" className="align-middle"/> : undefined }
-				{ contract.product == 'bfr' ? <img src="/bfr.png" height="40" width="40" alt="BFR" className="align-middle"/> : undefined }
-				{ contract.product == 'sor' ? <img src="/sor.png" height="40" width="40" alt="Sub-orbital rocket" className="align-middle"/> : undefined }
+				{ contract.product == 'sep' ? <img src="sep.png" height="40" width="40" alt="Single Engine Plane" className="align-middle"/> : undefined }
+				{ contract.product == 'sat' ? <img src="sat.png" height="40" width="40" alt="Satellite" className="align-middle"/> : undefined }
+				{ contract.product == 'lux' ? <img src="lux.png" height="40" width="40" alt="Luxury jet" className="align-middle"/> : undefined }
+				{ contract.product == 'jumbo' ? <img src="jumbo.png" height="40" width="40" alt="Jumbo jet" className="align-middle"/> : undefined }
+				{ contract.product == 'bfr' ? <img src="bfr.png" height="40" width="40" alt="BFR" className="align-middle"/> : undefined }
+				{ contract.product == 'sor' ? <img src="sor.png" height="40" width="40" alt="Sub-orbital rocket" className="align-middle"/> : undefined }
 				{ contract.product == '' ? <div className="box40"/> : undefined }
 				
 				<select name="product" value={contract.product} onChange={setProduct} placeholder="Product">
